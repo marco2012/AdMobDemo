8b7cffb0e6ebb97715c4da3bd040206e276d1219
Marco Lupia <marcolupia@google.com>
2025-09-16 15:46:23 +0100

    title

--- app/src/main/java/com/pankti/googleadsdemo/MainActivity.kt
+++ app/src/main/java/com/pankti/googleadsdemo/MainActivity.kt
@@ -21,14 +21,14 @@ class MainActivity : AppCompatActivity() {
     private fun performClicks() {
         binding.btnBannerAds.setOnClickListener { startActivity(Intent(this, BannerAdsActivity::class.java)) }
         binding.btnAdaptiveBannerAds.setOnClickListener { startActivity(Intent(this, AdaptiveBannerActivity::class.java)) }
-        binding.btnInterstitialAds.setOnClickListener { startActivity(Intent(this, InterstitialAdsActivity::class.java)) }
+        /*binding.btnInterstitialAds.setOnClickListener { startActivity(Intent(this, InterstitialAdsActivity::class.java)) }
         binding.btnNativeAds.setOnClickListener { startActivity(Intent(this, NativeAdsActivity::class.java)) }
         binding.btnRewardedAds.setOnClickListener { startActivity(Intent(this, RewardedAdsActivity::class.java)) }
         binding.btnRewardedInterstitialAds.setOnClickListener { startActivity(Intent(this, RewardedInterstitialAdActivity::class.java)) }
 
         binding.btnAppOpenAds.setOnClickListener {
             Toast.makeText(this, "It will show when your app launch", Toast.LENGTH_LONG).show()
-        }
+        }*/
     }
 
 }
